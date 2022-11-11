435a30f64aaa317a8ab6d30e73abbe3506ffb0a5
Mike Bennett <mvbennett9@gmail.com>
2022-11-10 21:29:29 -0500

    made a cash register object to control everything

--- app.js
+++ app.js
@@ -4,115 +4,155 @@ const makeObj = (cid) => {
   return obj;
 }
 
+const regi = (sum, cidObj) => {
+  const obj = {
+    sum: sum,
+    status: '',
+    diff: {
+      "ONE HUNDRED": 0,
+      "TWENTY": 0,
+      "TEN": 0,
+      "FIVE": 0,
+      "ONE": 0,
+      "QUARTER": 0,
+      "DIME": 0,
+      "NICKEL": 0,
+      "PENNY": 0
+    },
+    cidObj: cidObj
+  }
+  return obj;
+}
+
+const makeChange = (sum, obj) => {
+  const regiObj = regi(sum, obj);
+
+  let h = 0;
+  while(regiObj.sum >= 100 && regiObj.cidObj["ONE HUNDRED"] >= 100) {
+    regiObj.sum -= 100;
+    regiObj.cidObj["ONE HUNDRED"] -= 100;
+    h += 100;
+    regiObj.diff["ONE HUNDRED"] += 100;
+  }
+
+  let tw = 0;
+  while(regiObj.sum >= 20 && regiObj.cidObj.TWENTY) {
+    regiObj.sum -= 20;
+    regiObj.cidObj.TWENTY -= 20;
+    tw += 20;
+    regiObj.diff["TWENTY"] = tw;
+  }
+
+  let ten = 0;
+  while(regiObj.sum >= 10 && regiObj.cidObj.TEN >= 10){
+    regiObj.sum -= 10;
+    regiObj.cidObj.TEN -= 10;
+    ten += 10;
+    regiObj.diff["TEN"] = ten;
+  }
+
+  let f = 0;
+  while(regiObj.sum >= 5 && regiObj.cidObj.FIVE >= 5) {
+    regiObj.sum -= 5;
+    regiObj.cidObj.FIVE -= 5;
+    f += 5;
+    regiObj.diff["FIVE"] = f;
+  }
+
+  let o = 0;
+  while(regiObj.sum >= 1 && regiObj.cidObj.ONE >= 1) {
+    regiObj.sum -= 1;
+    regiObj.cidObj.ONE -= 1;
+    o += 1;
+    regiObj.diff["ONE"] = o;
+
+  }
+  let q = 0;
+  while(regiObj.sum >= 0.25 && regiObj.cidObj.QUARTER >= 0.25){
+    regiObj.sum -= .25;
+    regiObj.cidObj.QUARTER -= .25;
+    q += .25;
+    regiObj.diff["QUARTER"] = q;
+  }
+  let d = 0;
+  while(regiObj.sum >= .1 && regiObj.cidObj.DIME >= .1){
+    regiObj.sum -= .1;
+    regiObj.cidObj.DIME -= .1;
+    d += .1;
+    regiObj.diff["DIME"] =d;
+  }
+
+  let n = 0;
+  while(regiObj.sum >= .05 && regiObj.cidObj.NICKEL >= .05){
+    regiObj.sum -= .05;
+    regiObj.cidObj.NICKEL -.05;
+    n += .05
+    regiObj.diff["NICKEL"] = n;
+  }
+
+  let p = 0;
+  while(regiObj.sum >= .001 && regiObj.cidObj.PENNY >= .001){
+    regiObj.sum = Math.round(regiObj.sum * 100)/100;
+    regiObj.sum -= .01;
+    regiObj.cidObj.PENNY -= .01;
+    p += .01;
+    regiObj.diff["PENNY"] = Math.round(p*100)/100;
+  }
+
+  let remain = 0;
+  Object.values(obj).forEach(val => remain += val);
+
+  let newObj = {};
+  if(regiObj.sum > 0.00){
+    regiObj.status = "INSUFFICIENT_FUNDS"
+  } else if(regiObj.sum > -1 && remain > 0){
+    regiObj.status = "OPEN";
+    Object.keys(regiObj.diff).map((key) => {
+      if (regiObj.diff[key] > 0) {
+        newObj[key] = regiObj.diff[key]
+      }
+    })
+    // console.log(newObj);
+  } else if(regiObj.sum > -1 && remain > -1){
+    regiObj.status = "CLOSED";
+    newObj = regiObj.diff;
+    // regiObj.diff = {};
+    // regiObj.diff["PENNY"] = Math.floor(p*100)/100;
+    // regiObj.diff["NICKEL"] = n;
+    // regiObj.diff["DIME"] = d;
+    // regiObj.diff["QUARTER"] = q;
+    // regiObj.diff["ONE"] = o;
+    // regiObj.diff["FIVE"] = f;
+    // regiObj.diff["TEN"] = ten;
+    // regiObj.diff["TWENTY"] = tw;
+    // regiObj.diff["ONE HUNDRED"] = h;
+    }
+  let change = [];
+  if (regiObj.status === "OPEN" || regiObj.status === "INSUFFICIENT FUNDS") {
+    for(let j = 0; j < Object.keys(newObj).length; j++){
+      change.push([Object.keys(newObj)[j], Object.values(newObj)[j]]);
+    }
+  } else if (regiObj.status === "CLOSED") {
+      for(let j = 0; j < Object.keys(newObj).length; j++){
+        change.unshift([Object.keys(newObj)[j], Object.values(newObj)[j]]);
+  }}
+  let result = {
+    "status": regiObj.status,
+    "change": change
+  }
+  return result;
+}
+
 const checkCashRegister = (price, cash, cid) => {
   let cidObj = makeObj(cid);
 
   let sum = cash - price;
-  function makeChange(sum, obj){
-    let status = "";
-    let diff = {};
-    let h = 0;
-    while(sum >= 100 && obj["ONE HUNDRED"] >= 100) {
-      sum -= 100;
-      obj["ONE HUNDRED"] -= 100;
-      h += 100;
-      diff["ONE HUNDRED"] = h;
-    }
 
-    let tw = 0;
-    while(sum >= 20 && obj.TWENTY) {
-      sum -= 20;
-      obj.TWENTY -= 20;
-      tw += 20;
-      diff["TWENTY"] = tw;
-    }
-
-    let ten = 0;
-    while(sum >= 10 && obj.TEN >= 10){
-      sum -= 10;
-      obj.TEN -= 10;
-      ten += 10;
-      diff["TEN"] = ten;
-    }
-
-    let f = 0;
-    while(sum >= 5 && obj.FIVE >= 5) {
-      sum -= 5;
-      obj.FIVE -= 5;
-      f += 5;
-      diff["FIVE"] = f;
-    }
-
-    let o = 0;
-    while(sum >= 1 && obj.ONE >= 1) {
-      sum -= 1;
-      obj.ONE -= 1;
-      o += 1;
-      diff["ONE"] = o;
-
-    }
-    let q = 0;
-    while(sum >= 0.25 && obj.QUARTER >= 0.25){
-      sum -= .25;
-      obj.QUARTER -= .25;
-      q += .25;
-      diff["QUARTER"] = q;
-    }
-    let d = 0;
-    while(sum >= .1 && obj.DIME >= .1){
-      sum -= .1;
-      obj.DIME -= .1;
-      d += .1;
-      diff["DIME"] =d;
-    }
-    let n = 0;
-    while(sum >= .05 && obj.NICKEL >= .05){
-      sum -= .05;
-      obj.NICKEL -.05;
-      n += .05
-      diff["NICKEL"] = n;
-    }
-    let p = 0;
-    while(sum >= .001 && obj.PENNY >= .001){
-      sum = Math.round(sum * 100)/100;
-      sum -= .01;
-      obj.PENNY -= .01;
-      p += .01;
-      diff["PENNY"] = Math.round(p*100)/100;
-    }
-
-    let remain = 0;
-    for(let i = 0; i < Object.values(obj).length; i++){
-      remain += Object.values(obj)[i];
-    }
-
-    if(sum > 0.00){
-      status = "INSUFFICIENT_FUNDS"
-      diff = {};
-    } else if(sum > -1 && remain > 0){
-      status = "OPEN"
-    } else if(sum > -1 && remain > -1){
-      status = "CLOSED";
-      diff = {};
-      diff["PENNY"] = Math.round(p*100)/100;
-      diff["NICKEL"] = n;
-      diff["DIME"] = d;
-      diff["QUARTER"] = q;
-      diff["ONE"] = o;
-      diff["FIVE"] = f;
-      diff["TEN"] = ten;
-      diff["TWENTY"] = tw;
-      diff["ONE HUNDRED"] = h;
-      }
-    let change = [];
-    for(let j = 0; j < Object.keys(diff).length; j++){
-      change.push([Object.keys(diff)[j], Object.values(diff)[j]]);
-    }
-    let result = {
-      "status": status,
-      "change": change
-    }
-    return result;
-  }
   return makeChange(sum, cidObj);
 }
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
